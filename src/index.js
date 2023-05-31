d5654a2fbe24aac00ff0f17695fbc677b5dd30a2
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2023-05-31 14:43:25 -0500

    Added files
    Added files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './navbar.css'; // Import the navbar.css file here
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
